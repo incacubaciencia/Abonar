f34825c126619426b9796f9377ac5fed6991970d
Daniel Fernandez Sotolongo <dfedez@inca.edu.cu>
2026-08-26 16:24:40 -0400

    Version 1.0.7 corrigiendo errores de la version 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "cu.edu.inca.abonosverdes"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.6"
+        versionCode = 4
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
