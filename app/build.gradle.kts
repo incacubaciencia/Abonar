80a9bd0575f7502b8d26b9f682cb36b4b1403ef9
Daniel Fernandez Sotolongo <dfedez@inca.edu.cu>
2026-08-26 16:34:59 -0400

    Fix: Forzar creación de releases independientes por tag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "cu.edu.inca.abonosverdes"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.7"
+        versionCode = 5
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
